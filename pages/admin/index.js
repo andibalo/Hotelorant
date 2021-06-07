369b0d1eda064ca3fdafef29a79b6328715dbe2d
andi balo <andibalo213@gmail.com>
2021-06-07 20:48:56 +0700

    created and styled hotel detail page, edit profile page

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,3 +1,14 @@
+import { HeaderTab } from "../../components/admin/header-tab";
+import { Container } from "../../components/atoms/container";
+import { Navbar } from "../../components/navbar";
+
 export default function AdminHome() {
-  return <div>AdminHome</div>;
+  return (
+    <div>
+      <Navbar />
+      <Container>
+        <HeaderTab route="home" />
+      </Container>
+    </div>
+  );
 }
