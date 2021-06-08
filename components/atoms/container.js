14d24e28d14fec4d1d2390a33dcfc508cc908643
hernandomartin12 <75068960+hernandomartin12@users.noreply.github.com>
2021-06-08 20:56:41 +0700

    Booking3
    dah kelar

--- components/atoms/container.js
+++ components/atoms/container.js
@@ -2,7 +2,7 @@ import { Container as ChakraContainer } from "@chakra-ui/react";
 
 export const Container = ({ children, noPads }) => {
   return (
-    <ChakraContainer maxW="container.xl" py={noPads ? "0" : "20"}>
+    <ChakraContainer maxW="container.xl" py={noPads ? "0" : "10"}>
       {children}
     </ChakraContainer>
   );
